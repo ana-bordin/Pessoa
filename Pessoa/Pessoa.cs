db7c24484a2019129baf15aecf2da49570d2004d
AnnyaB <anabordin2008@gmail.com>
2024-04-29 12:09:54 -0300

    Adicionar arquivos de projeto.

---
+++ Pessoa/Pessoa.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Pessoa
+{
+    public string Nome;
+    public string Sobrenome;
+    public int Idade;
+    public string TelefoneFixo;
+    public string TelefoneCelular;
+    public string Email;
+
+    public Pessoa()
+    {
+    }
+    public string DefinirNome()
+    {
+        Console.WriteLine("\nInforme o nome desejado:");
+        return Console.ReadLine();
+    }
+    public string DefinirSobrenome()
+    {
+        Console.WriteLine("Informe o sobrenome:");
+        return Console.ReadLine();
+    }
+    public int DefinirIdade()
+    {
+        Console.WriteLine("Informe a idade:");
+        return int.Parse(Console.ReadLine());
+    }
+    public void DefinirTelefoneFixo(string telefoneFixo)
+    {
+        this.TelefoneFixo = telefoneFixo;
+    }
+    public void DefinirTelefoneCelular(string telefoneCelular)
+    {
+        TelefoneCelular = telefoneCelular;
+    }
+    public void DefinirEmail(string email)
+    {
+        Email = email;
+    }
+    public void MostrarDados()
+    {
+        Console.WriteLine($"\n\nNome: {this.Nome}");
+        Console.WriteLine($"Sobrenome: {this.Sobrenome}");
+        Console.WriteLine($"Idade: {this.Idade}");
+        Console.WriteLine($"TelefoneFixo: {this.TelefoneFixo}");
+        Console.WriteLine($"TelefoneCelular: {this.TelefoneCelular}");
+        Console.WriteLine($"Email: {this.Email}");
+    }
+}
---
+++ Pessoa/Program.cs
@@ -0,0 +1,31 @@
+﻿string telefoneFixo, telefoneCelular, email;
+Pessoa[] pessoas = new Pessoa[3];
+
+Pessoa CriarPessoa()
+{
+    Pessoa pessoa = new Pessoa();
+
+    pessoa.Nome = pessoa.DefinirNome();
+    pessoa.Sobrenome = pessoa.DefinirSobrenome();
+    pessoa.Idade = pessoa.DefinirIdade();
+
+    Console.WriteLine("Informe o telefone fixo:");
+    telefoneFixo = Console.ReadLine();
+    pessoa.DefinirTelefoneFixo(telefoneFixo);
+
+    Console.WriteLine("Informe o telefone celular:");
+    telefoneCelular = Console.ReadLine();
+    pessoa.DefinirTelefoneCelular(telefoneCelular);
+
+    Console.WriteLine("Informe o email:");
+    email = Console.ReadLine();
+    pessoa.DefinirEmail(email);
+
+    return pessoa;
+}
+
+for (int i = 0; i < 3; i++)
+    pessoas[i] = CriarPessoa();
+
+for (int i = 0; i < 3; i++)
+    pessoas[i].MostrarDados();

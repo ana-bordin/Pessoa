28da347023b76b12990e759328c054d78133c934
AnnyaB <anabordin2008@gmail.com>
2024-04-30 11:18:16 -0300

    encapsulamento

---
+++ PessoaPOO/Pessoa.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PessoaPOO
+{
+    internal class Pessoa
+    {
+        string Nome;
+        int Idade;
+
+        public Pessoa() { }
+        public Pessoa(string nome, int idade)
+        {
+            this.Nome = nome;
+            this.Idade = idade;
+        }
+
+        public void setNome(string nome)
+        {
+            this.Nome = nome;
+        }
+        public string getNome()
+        {
+            return this.Nome;
+        }
+        public void setIdade(int idade)
+        {
+            this.Idade = idade;
+        }
+        public int getIdade()
+        {
+            return this.Idade;
+        }
+    }
+}
---
+++ PessoaPOO/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace PessoaPOO
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa();
+            p.setNome(Console.ReadLine());
+            p.setIdade(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine(p.getNome());
+            Console.WriteLine(p.getIdade());
+        }
+    }
+}
